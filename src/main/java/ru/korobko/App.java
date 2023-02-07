0fe597d2f0e4eb612e20864b5caa8e01492d1da8
korobko <19kvd95@mail.ru>
2023-02-07 23:05:15 +0300

    fix empty element check

--- src/main/java/ru/korobko/App.java
+++ src/main/java/ru/korobko/App.java
@@ -12,7 +12,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        groupLinesFromFile("test.txt");
+        groupLinesFromFile("lng.txt");
     }
 
     public static void groupLinesFromFile(String fileName) {
@@ -25,15 +25,17 @@ public class App
 
             //при добавлении в группу значение строки в общем списке перетирается
             //если во внешний цикл попадает не пустая строка, значит в группах её нет
-            if (!lines.get(i).isEmpty()) {
+            if (!lines.get(i).isBlank()) {
                 List<String> group = new ArrayList<>();
                 group.add(lines.get(i));
                 //внутренний цикл проходит по всем следующим строкам, проверяя на соответствие заданному условию
                 //(совпадение значений в колонке)
-                    lines.subList(i + 1, lines.size()).parallelStream().forEachOrdered(line -> {
-                        if (lineIsInsideTheGroup(group, line)) {
-                            group.add(line);
-                            lines.set(lines.indexOf(line), "");
+                AtomicInteger count = new AtomicInteger(i + 1);
+                    lines.subList(i + 1, lines.size()).parallelStream().forEachOrdered(l -> {
+                        if (lineIsInsideTheGroup(group, l)) {
+                            group.add(l);
+                            lines.set(count.get(), "");
+                            count.getAndIncrement();
                         }
                     });
                 if (group.size() > 1) {
@@ -58,14 +60,16 @@ public class App
         for (int i = 0; i < groups.size(); i++) {
             String string1 = groups.get(i);
             //сравниваем каждую строку группы с проверяемой строкой и проходим циклом по короткой во избежание IndexOutOfBoundsException
-            String[] shortList = string1.split(";").length < string2.split(";").length ?
-                    string1.split(";")
-                    : string2.split(";");
-            String[] longList = string2.split(";").length > string1.split(";").length ?
-                    string2.split(";")
-                    : string1.split(";");
+            String[] split1 = string1.split(";");
+            String[] split2 = string2.split(";");
+            String[] shortList = split1.length < split2.length ?
+                    split1
+                    : split2;
+            String[] longList = split1.length >= split2.length ?
+                    split1
+                    : split2;
             for (int j = 0; j < shortList.length; j++) {
-                if (!shortList[j].isBlank() && shortList[j].equals(longList[j])) {
+                if (!shortList[j].equals("\"\"") && shortList[j].equals(longList[j])) {
                     return true;
                 }
             }
